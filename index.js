637407b285f1cdcdb670dc4ab5eed33639bbcef5
zitXus <johansvalberg619@gmail.com>
2020-12-22 08:52:47 +0100

    Added copyright
    Added copyright and autoscroll

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ $(".home_btn").on('click', function(event){
     event.stopImmediatePropagation();
     location.reload();
 });
+
+$(".contact_btn, .about_btn, .games_btn").on('click', function(event){
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    $("html, body").animate({ scrollTop: $(document).height() }, 800);
+});
+
+var d = new Date(); page.currentYear = d.getFullYear(); 
